d8f082248066d574668fc81aa3a3625aaa739ec8
Mathel-git <mathewajahobasi.com>
2025-12-13 07:46:43 +0100

    Ts-Course

---
+++ Functions.js
@@ -0,0 +1,47 @@
+//function calculateinterest() { 
+    //console.log("I just call a function to return a name");
+//}
+//console.log(calculateinterest());
+    
+//const calculateinterest = () => {
+    //console.log("I just call a function to return a name");
+    //return "Mathew Kol"
+//}
+
+//console.log(calculateinterest());
+
+/*function sumNumbers(a, b) {
+    const sum = a + b;
+    return sum;
+}
+
+console.log(sumNumbers(20, 30));
+console.log(sumNumbers(100, 200));*/
+
+/*function calculateinterest(principal, annualRate, days) {
+    const interest = principal * (annualRate / 100) * (days / 365);
+    return interest;
+    console.log("the interest is: " + interest);
+}
+console.log(calculateinterest(1000, 5, 300));*/
+
+function checkName(name) {
+    if (name === "Mathew Kol") {
+        return "Mathew kol!";
+    } else {
+        return false;
+    }
+}
+
+function checkResponsefromcheckName() { 
+    let response = checkName("Mathew Kol");
+    
+    if (response) {
+        return "Welcome back, Mathew Kol!"
+        } else {
+            return "false";
+        }
+}
+
+    console.log(checkResponsefromcheckName());
+ 
---
+++ HelloWorld.js
@@ -0,0 +1,26 @@
+// var, let, const
+//var greeting = "Hello, world";
+//const greeting = "Hello, world"
+//let greeting = "Hello, world";
+//console.log (greeting);
+
+let firstnum = 5;
+let secondnum = 10;
+let result = firstnum + secondnum;
+console.log(result);
+
+var age = 25;
+var name = "Alice";
+
+var array = [1, 2, 3, 4, 5];
+var studentarray = [
+    {name: "John", age: 20},
+    {name: "Jane", age: 22},
+    {name: "Jim", age: 24}
+];
+for (var i = 0; i < array.length; i++) {
+    console.log(array.length);
+}
+console.log("StudentArray.length: " + studentarray.length);
+console.log("Name: " + name + ", Age: " + age +",  sum:" + result);
+//console.log(result);
